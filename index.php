cae45485a8ab3884e49841e68f33e0a152f8189d
iptime-fox <dudndi826@gmail.com>
2023-05-17 12:45:34 +0900

    관리자 페이지 작성중 0517

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   // print_r($path_name);
   // print_r($path_name[2]);
   $inc_adrs_post = ['signup', 'signin', 'is_signin']; // post로 저달되는 주소
-  $inc_adrs_get = ['signout']; // get으로 전달되는 주소
+  $inc_adrs_get = ['signout', 'check_admin_signin']; // get으로 전달되는 주소
 
   for($i = 0; $i < count($inc_adrs_post); $i++){
     // echo $inc_adrs_post[$i];
@@ -17,13 +17,14 @@
     } 
   }
 
-  if($path_name[2] == 'register' && $path_name[3] == 'signout' && $_SERVER['REQUEST_METHOD'] == "GET"){
-    include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/signout.php';
-  } 
+  for($i = 0; $i < count($inc_adrs_get); $i++){
+    if($path_name[2] == 'register' && $path_name[3] == $inc_adrs_get[$i] && $_SERVER['REQUEST_METHOD'] == "GET"){
+      include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/'.$inc_adrs_get[$i].'.php';
+    } 
+  }
 
-  if(!in_array($path_name[3], $inc_adrs_post) && $path_name[3] != 'signout'){ // 배열에 없는 문자열이 나오면 index.php로 전달
+  if(!in_array($path_name[3], $inc_adrs_post) && !in_array($path_name[3] , $inc_adrs_get)){ // 배열에 없는 문자열이 나오면 index.php로 전달
     echo 'index.php';
-
   }
 
   
