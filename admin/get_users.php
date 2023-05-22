693ff06b17fa46220389922127e954e7770839e9
iptime-fox <dudndi826@gmail.com>
2023-05-22 11:53:44 +0900

    회원관리 기능 : 회원 불러오기 완료 0522

--- admin/get_users.php
+++ admin/get_users.php
@@ -12,28 +12,33 @@
 
   $stmt = $get_users->get_users();
 
-  // $user_arr = [];
-  // $user_num = $stmt->rowCount();
-
-  // if($user_num > 0){
-  //   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-  //     extract($row);
-  //     $user_info = [
-  //       'user_idx'  => $row['user_idx'],
-  //       'user_id'   => $row['user_id'],
-  //       'user_name' => $row['user_name'],
-  //       'user_lvl'  => $row['user_lvl']
-  //     ];
-  //     array_push($user_arr, $user_info);
-  //   }
   
-  //   $msg = $user_arr;
-  // } else {
-  //   $msg = ['msg' => '회원이 존재하지 않습니다.'];
-  // }
+  if(!$get_users->get_users()){
+    $msg = ['msg' => '권한이 없는 사용자 입니다.'];
+  } else {
+    $user_arr = [];
+    $user_num = $stmt->rowCount();
 
+    if($user_num > 0){
+      while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $user_info = [
+          'user_idx'  => $row['user_idx'],
+          'user_id'   => $row['user_id'],
+          'user_name' => $row['user_name'],
+          'user_lvl'  => $row['user_lvl']
+        ];
+        array_push($user_arr, $user_info);
+      }
+    
+      $msg = $user_arr;
+    } else {
+      array_push($user_arr, null);
+      $msg = $user_arr;
+      
+    }
+  }
   
-  // echo json_encode($msg);
-  // echo $stmt->rowCount();
+  echo json_encode($msg);
 
 ?>
