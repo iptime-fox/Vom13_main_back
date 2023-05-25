4f3e5bea9f262d4b22771bd7846c37e54fc0d977
iptime-fox <dudndi826@gmail.com>
2023-05-25 11:37:36 +0900

    상품 입력 기능 완료 0525

--- product/insert_product.php
+++ product/insert_product.php
@@ -6,14 +6,16 @@
   header('Access-Control-Allow-Headers: Access_Control-Allow_headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-Width');
 
   include $_SERVER["DOCUMENT_ROOT"].'/connect/db_conn.php';
-  include $_SERVER["DOCUMENT_ROOT"].'/baexang_back/product/product.php';
+  include_once $_SERVER["DOCUMENT_ROOT"].'/baexang_back/product/product.php';
 
   // echo $_POST['pr_img'], $_POST['pr_ttl'], $_POST['pr_wt_en'], $_POST['pr_wt_kr'], $_POST['pr_pri'], $_POST['pr_desc'], $_POST['pr_type'];
 
 
   $msg = [];
+  $upload_dir = ''; // 사진 저장 파일
 
   $insert_product = new Product($db);
+
 
   // 포스트 텍스트 변수 클래스 연결
   $insert_product->pr_ttl     = $_POST['pr_ttl'];
@@ -22,6 +24,14 @@
   $insert_product->pr_pri     = $_POST['pr_pri'];
   $insert_product->pr_desc    = $_POST['pr_desc'];
   $insert_product->pr_type    = $_POST['pr_type'];
+
+  // 테이블 이동 분개
+  if($_POST['pr_type'] == 'picture'){
+    $upload_dir = 'upload_pp_img';
+  } else {
+
+    $upload_dir = 'upload_dp_img';
+  }
 
   // 이미지 파일 처리
   $ext_n_arr = explode('.', $_FILES['pr_img']['name']);
@@ -36,12 +46,21 @@
   $insert_product->pr_img = $img_f_name; // 클래스로 보내는 최종 이미지 파일명
 
   // 이미지 사이즈 제한
-  if($img_t_size > 1000000){ // 1MB
+  if($img_t_size > 3000000){ // 1MB
     $msg=['msg' => '파일 용량이 1MB를 초과했습니다.'];
   } else if(!in_array($img_n_end_name, $allowed_exp)){ // $allowed_exp 배열 안에 $img_n_end_name 문자열이 없다면 true
     $msg = ['msg' => '허용되지 않는 파일 형식입니다.'];
   } else {
-    $msg = ['msg' => '상품 입력 준비 완료!!'];
+    // copy 참조 https://solbel.tistory.com/1688
+    // $msg = ['msg' => '상품 입력 준비 완료!!'];
+    copy($_FILES['pr_img']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/baexang_front/images/'.$upload_dir.'/'.$img_f_name);
+
+    // 입력 성공 시 처리
+    if(!$insert_product->insert_product()){
+      $msg = ['msg' => '작품 입력에 실패했습니다.'];
+    } else {
+      $msg = ['msg' => '작품이 입력 되었습니다.'];
+    }
   }
 
   echo json_encode($msg);
--- product/product.php
+++ product/product.php
@@ -2,7 +2,7 @@
 
   class Product{
     private $conn;
-    private $table = 'bx_user';
+    private $table;
 
     public $pr_img;  
     public $pr_ttl;    
@@ -11,13 +11,46 @@
     public $pr_pri;
     public $pr_desc;
     public $pr_type;
+    public $pr_reg;
+    public $pr_ID;
 
     public function __construct($db){
       $this->conn = $db;
     }
 
-    public function insert_prodect(){
-      return true;
+    public function insert_product(){
+      // 타입에 따른 테이블 분개
+      if($this->pr_type == 'picture'){
+        $this->table = 'bx_pp';
+      } else {
+        $this->table = 'bx_dp';
+      }
+
+      $sql = "INSERT INTO ".$this->table." SET bx_img=:pr_img, bx_ttl=:pr_ttl, bx_wt_en=:pr_wt_en, bx_wt_kr=:pr_wt_kr, bx_pri=:pr_pri, bx_desc=:pr_desc, bx_reg =:pr_reg, bx_ID=:pr_ID";
+
+      $stmt = $this->conn->prepare($sql);
+
+      $this->pr_img   = htmlspecialchars(strip_tags($this->pr_img));
+      $this->pr_ttl   = htmlspecialchars(strip_tags($this->pr_ttl));
+      $this->pr_wt_en = htmlspecialchars(strip_tags($this->pr_wt_en));
+      $this->pr_wt_kr = htmlspecialchars(strip_tags($this->pr_wt_kr));
+      $this->pr_pri   = htmlspecialchars(strip_tags($this->pr_pri));
+      $this->pr_desc  = nl2br($this->pr_desc);
+      $this->pr_reg   = date("Y-m-d H:i:s"); // 년월일 시간분초
+      $this->pr_ID    = mt_rand(1, 1000000); // 1 1백만 사이 랜덤 숫자
+
+      $stmt->bindParam(':pr_img',   $this->pr_img);
+      $stmt->bindParam(':pr_ttl',   $this->pr_ttl );
+      $stmt->bindParam(':pr_wt_en', $this->pr_wt_en);
+      $stmt->bindParam(':pr_wt_kr', $this->pr_wt_kr);
+      $stmt->bindParam(':pr_pri',   $this->pr_pri);
+      $stmt->bindParam(':pr_desc',  $this->pr_desc);
+      $stmt->bindParam(':pr_reg',   $this->pr_reg);
+      $stmt->bindParam(':pr_ID',    $this->pr_ID);
+
+      $result = $stmt->execute();
+
+      return $result ? true : false;
     }
 
 
